03c7ec6c8a318f7cccc9a19eaee88a3b78a45cc6
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-12-30 01:31:57 -0500

    Add Form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './styles.css';
+import './styles/styles.css';
+import Form from './components/Form';
 
 class App extends React.Component {
   constructor() {
@@ -9,7 +10,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='columns-2'>
-        <div className='flex justify-center'>HEY</div>
+        <Form />
         <div className='flex justify-center'>BYE</div>
       </div>
     )
---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/styles.css';
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='flex justify-center'>HEY THERE</div>
+        )
+    }
+}
+
+export default Form;
