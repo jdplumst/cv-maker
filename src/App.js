0e15dac175cd716949160d165be748104435c88c
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-12-30 14:43:31 -0500

    Remove helper methods for updatePersonalInfo

--- src/App.js
+++ src/App.js
@@ -34,21 +34,16 @@ class App extends React.Component {
     this.updateEducation = this.updateEducation.bind(this);
   };
   
-  // Calls correct method to update personal information state
+  // Updates personal information state
   updatePersonalInfo(e) {
-    if (e.target.name === 'firstname') {
-      this.updateFirstName(e);
-    } else if (e.target.name === 'lastname') {
-      this.updateLastName(e);
-    } else if (e.target.name === 'phone') {
-      this.updatePhone(e);
-    } else if (e.target.name === 'email') {
-      this.updateEmail(e);
-    } else if (e.target.name === 'linkedin') {
-      this.updateLinkedin(e);
-    } else if (e.target.name === 'github') {
-      this.updateGithub(e);
-    }
+    this.setState(prevState => {
+      let personalInfo = Object.assign({}, prevState.personalInfo);
+      personalInfo[e.target.name] = e.target.value;
+      return ({
+        ...prevState,
+        personalInfo
+      })
+    })
   };
 
   // Calls correct method to update education state
@@ -56,102 +51,6 @@ class App extends React.Component {
     if (e.target.name === 'school') {
       this.updateSchool(e);
     }
-  };
-
-  updateFirstName(e) {
-    this.setState((prevState) => {
-      return({
-        ...prevState,
-        personalInfo: {
-          firstname: e.target.value,
-          lastname: this.state.personalInfo.lastname,
-          phone: this.state.personalInfo.phone,
-          email: this.state.personalInfo.email,
-          linkedin: this.state.personalInfo.linkedin,
-          github: this.state.personalInfo.github,
-        }
-      })
-    })
-  };
-
-  updateLastName(e) {
-    this.setState((prevState) => {
-      return({
-        ...prevState,
-        personalInfo: {
-          firstname: this.state.personalInfo.firstname,
-          lastname: e.target.value,
-          phone: this.state.personalInfo.phone,
-          email: this.state.personalInfo.email,
-          linkedin: this.state.personalInfo.linkedin,
-          github: this.state.personalInfo.github,
-        }
-      })
-    })
-  };
-
-  updatePhone(e) {
-    this.setState((prevState) => {
-      return({
-        ...prevState,
-        personalInfo: {
-          firstname: this.state.personalInfo.firstname,
-          lastname: this.state.personalInfo.lastname,
-          phone: e.target.value,
-          email: this.state.personalInfo.email,
-          linkedin: this.state.personalInfo.linkedin,
-          github: this.state.personalInfo.github,
-        }
-      })
-    })
-  };
-
-  updateEmail(e) {
-    this.setState((prevState) => {
-      return({
-        ...prevState,
-        personalInfo: {
-          firstname: this.state.personalInfo.firstname,
-          lastname: this.state.personalInfo.lastname,
-          phone: this.state.personalInfo.phone,
-          email: e.target.value,
-          linkedin: this.state.personalInfo.linkedin,
-          github: this.state.personalInfo.github,
-        }
-      })
-    })
-  };
-
-  updateLinkedin(e) {
-    this.setState((prevState) => {
-      return({
-        ...prevState,
-        personalInfo: {
-          firstname: this.state.personalInfo.firstname,
-          lastname: this.state.personalInfo.lastname,
-          phone: this.state.personalInfo.phone,
-          email: this.state.personalInfo.email,
-          linkedin: e.target.value,
-          github: this.state.personalInfo.github,
-        }
-      })
-    })
-  };
-
-  updateGithub(e) {
-    this.setState((prevState) => {
-      return({
-        ...prevState,
-        personalInfo: {
-          firstname: this.state.personalInfo.firstname,
-          lastname: this.state.personalInfo.lastname,
-          phone: this.state.personalInfo.phone,
-          email: this.state.personalInfo.email,
-          linkedin: this.state.personalInfo.linkedin,
-          github: e.target.value,
-        }
-      })
-    })
   };
 
   updateSchool(e) {
