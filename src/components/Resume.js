4f02ae6dc237f6952af67ce8fa2d2e4ca6f3aec1
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-04 00:35:44 -0500

    Convert Resume to functional component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ResumePersonalInfo from './ResumePersonalInfo';
 import ResumeEducation from './ResumeEducation';
 import ResumeExperience from './ResumeExperience';
 import ResumeProjects from './ResumeProjects';
 import ResumeSkills from './ResumeSkills';
 
-class Resume extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className='flex justify-center'>
-                <div className='container m-10 bg-gray-100 shadow-lg p-5 h-fit overflow-auto'>
-                    <ResumePersonalInfo personalInfo={this.props.personalInfo} />
-                    <ResumeEducation education={this.props.education} />
-                    <ResumeExperience experience={this.props.experience} />
-                    <ResumeProjects projects={this.props.projects} />
-                    <ResumeSkills skills={this.props.skills} />
-                </div>
+function Resume(props) {
+    return (
+        <div className='flex justify-center'>
+            <div className='container m-10 bg-gray-100 shadow-lg p-5 h-fit overflow-auto'>
+                <ResumePersonalInfo personalInfo={props.personalInfo} />
+                <ResumeEducation education={props.education} />
+                <ResumeExperience experience={props.experience} />
+                <ResumeProjects projects={props.projects} />
+                <ResumeSkills skills={props.skills} />
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Resume;
