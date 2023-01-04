6673bf938500eceb57cae241db22e3a96cfcb6cf
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-04 00:40:16 -0500

    Convert ResumeEducation to functional component

--- src/components/ResumeEducation.js
+++ src/components/ResumeEducation.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ResumeEducation extends React.Component {
-    constructor(props) {
-        super(props);
-    };
-
-    render() {
-        return (
-            <div>
-                <h3 className='text-2xl mt-4'>Education</h3>
-                <hr className='border-black mb-4'></hr>
-                {this.props.education.map((education) => {
-                        return (
-                            <div className='grid grid-cols-2 mb-4' key={education.id}>
-                                <span><strong>{education.school}</strong></span>
-                                <span className='text-right'>{education.start} - {education.end}</span>
-                                <span>{education.degree} in {education.major}, Minor in {education.minor}</span>
-                                <span className='text-right'>{education.city}, {education.province}</span>
-                            </div>
-                        )
-                    })}
-            </div>
-        )
-    };
+function ResumeEducation(props) {
+    return (
+        <div>
+            <h3 className='text-2xl mt-4'>Education</h3>
+            <hr className='border-black mb-4'></hr>
+            {props.education.map((education) => {
+                    return (
+                        <div className='grid grid-cols-2 mb-4' key={education.id}>
+                            <span><strong>{education.school}</strong></span>
+                            <span className='text-right'>{education.start} - {education.end}</span>
+                            <span>{education.degree} in {education.major}, Minor in {education.minor}</span>
+                            <span className='text-right'>{education.city}, {education.province}</span>
+                        </div>
+                    )
+                })}
+        </div>
+    )
 }
 
 export default ResumeEducation;
