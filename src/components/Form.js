63b736aa078b7b6b079823a704226c970ae387f1
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-04 00:20:17 -0500

    Convert Form to functional component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,41 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import FormPersonalInfo from './FormPersonalInfo';
 import FormEducation from './FormEducation';
 import FormExperience from './FormExperience';
 import FormProjects from './FormProjects';
 import FormSkills from './FormSkills';
 
-class Form extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className='flex justify-center'>
-                <div className='container m-10 bg-gray-100 shadow-lg p-5 h-fit overflow-auto'>
-                    <FormPersonalInfo updatePersonalInfo={this.props.updatePersonalInfo} />
-                    <FormEducation education={this.props.education} 
-                                   updateEducation={this.props.updateEducation} 
-                                   addEducation={this.props.addEducation}
-                                   deleteEducation={this.props.deleteEducation} />
-                    <FormExperience experience={this.props.experience}
-                                    updateExperience={this.props.updateExperience}
-                                    addExperience={this.props.addExperience}
-                                    deleteExperience={this.props.deleteExperience}
-                                    addExperienceDescription={this.props.addExperienceDescription}
-                                    deleteExperienceDescription={this.props.deleteExperienceDescription} />
-                    <FormProjects projects={this.props.projects}
-                                  updateProjects={this.props.updateProjects}
-                                  addProject={this.props.addProject}
-                                  deleteProject={this.props.deleteProject}
-                                  addProjectDescription={this.props.addProjectDescription}
-                                  deleteProjectDescription={this.props.deleteProjectDescription} />
-                    <FormSkills updateSkills={this.props.updateSkills} />
-                </div>
+function Form(props) {
+    return (
+        <div className='flex justify-center'>
+            <div className='container m-10 bg-gray-100 shadow-lg p-5 h-fit overflow-auto'>
+                <FormPersonalInfo updatePersonalInfo={props.updatePersonalInfo} />
+                <FormEducation education={props.education} 
+                               updateEducation={props.updateEducation} 
+                               addEducation={props.addEducation}
+                               deleteEducation={props.deleteEducation} />
+                <FormExperience experience={props.experience}
+                                updateExperience={props.updateExperience}
+                                addExperience={props.addExperience}
+                                deleteExperience={props.deleteExperience}
+                                addExperienceDescription={props.addExperienceDescription}
+                                deleteExperienceDescription={props.deleteExperienceDescription} />
+                <FormProjects projects={props.projects}
+                              updateProjects={props.updateProjects}
+                              addProject={props.addProject}
+                              deleteProject={props.deleteProject}
+                              addProjectDescription={props.addProjectDescription}
+                              deleteProjectDescription={props.deleteProjectDescription} />
+                <FormSkills updateSkills={props.updateSkills} />
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+// class Form extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     render() {
+//         return (
+//             <div className='flex justify-center'>
+//                 <div className='container m-10 bg-gray-100 shadow-lg p-5 h-fit overflow-auto'>
+//                     <FormPersonalInfo updatePersonalInfo={this.props.updatePersonalInfo} />
+//                     <FormEducation education={this.props.education} 
+//                                    updateEducation={this.props.updateEducation} 
+//                                    addEducation={this.props.addEducation}
+//                                    deleteEducation={this.props.deleteEducation} />
+//                     <FormExperience experience={this.props.experience}
+//                                     updateExperience={this.props.updateExperience}
+//                                     addExperience={this.props.addExperience}
+//                                     deleteExperience={this.props.deleteExperience}
+//                                     addExperienceDescription={this.props.addExperienceDescription}
+//                                     deleteExperienceDescription={this.props.deleteExperienceDescription} />
+//                     <FormProjects projects={this.props.projects}
+//                                   updateProjects={this.props.updateProjects}
+//                                   addProject={this.props.addProject}
+//                                   deleteProject={this.props.deleteProject}
+//                                   addProjectDescription={this.props.addProjectDescription}
+//                                   deleteProjectDescription={this.props.deleteProjectDescription} />
+//                     <FormSkills updateSkills={this.props.updateSkills} />
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export default Form;
