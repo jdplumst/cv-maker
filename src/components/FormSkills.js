3eb055ffb03190884d6880d04aeb9559f56c2eb6
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-04 00:32:42 -0500

    Convert FormSkills to functional component

--- src/components/FormSkills.js
+++ src/components/FormSkills.js
@@ -1,32 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FormSkills extends React.Component {
-    constructor(props) {
-        super(props);
-    };
-
-    render() {
-        return (
-            <div>
-                <h2 className='text-3xl mb-4'>Personal Information</h2>
-                <label className='block'>Languages</label>
-                <textarea onInput={this.props.updateSkills} 
-                          name='languages' 
-                          placeholder='JavaScript, TypeScript, HTML, CSS' 
-                          className='w-full p-2'></textarea>
-                <label className='block'>Libraries and Frameworks</label>
-                <textarea onInput={this.props.updateSkills} 
-                          name='libraries' 
-                          placeholder='React, TailwindCSS, Sass, Node.js, Express' 
-                          className='w-full p-2'></textarea>
-                <label className='block'>Developer Tools</label>
-                <textarea onInput={this.props.updateSkills} 
-                          name='devtools' 
-                          placeholder='Git/GitHub, Linux, VS Code, MongoDB' 
-                          className='w-full p-2'></textarea>
-            </div>
-        )
-    };
+function FormSkills(props) {
+    return (
+        <div>
+            <h2 className='text-3xl mb-4'>Personal Information</h2>
+            <label className='block'>Languages</label>
+            <textarea onInput={props.updateSkills} 
+                      name='languages' 
+                      placeholder='JavaScript, TypeScript, HTML, CSS' 
+                      className='w-full p-2'></textarea>
+            <label className='block'>Libraries and Frameworks</label>
+            <textarea onInput={props.updateSkills} 
+                      name='libraries' 
+                      placeholder='React, TailwindCSS, Sass, Node.js, Express' 
+                      className='w-full p-2'></textarea>
+            <label className='block'>Developer Tools</label>
+            <textarea onInput={props.updateSkills} 
+                      name='devtools' 
+                      placeholder='Git/GitHub, Linux, VS Code, MongoDB' 
+                      className='w-full p-2'></textarea>
+        </div>
+    )
 }
 
 export default FormSkills;
