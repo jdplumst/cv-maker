3d948ea38afb5e38aeec848beea1ae5264e8bc63
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-04 00:43:50 -0500

    Convert ResumeProjects to functional component

--- src/components/ResumeProjects.js
+++ src/components/ResumeProjects.js
@@ -1,35 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ResumeProjects extends React.Component {
-    constructor(props) {
-        super(props);
-    };
-
-    render() {
-        return (
-            <div>
-                <h3 className='text-2xl mt-4'>Projects</h3>
-                <hr className='border-black mb-4'></hr>
-                {this.props.projects.map((project) => {
-                        return (
-                            <div key={project.id}>
-                                <div className='grid grid-cols-2'>
-                                    <span><strong>{project.project}</strong> | <em>{project.tech}</em></span>
-                                    <span className='text-right'>{project.link}</span>
-                                </div>
-                                <ul className="mb-4 list-disc list-inside">
-                                {project.descriptions.map((description) => {
-                                    return (
-                                        <li key={description.id}>{description.description}</li>
-                                    )
-                                })}
-                                </ul>
+function ResumeProjects(props) {
+    return (
+        <div>
+            <h3 className='text-2xl mt-4'>Projects</h3>
+            <hr className='border-black mb-4'></hr>
+            {props.projects.map((project) => {
+                    return (
+                        <div key={project.id}>
+                            <div className='grid grid-cols-2'>
+                                <span><strong>{project.project}</strong> | <em>{project.tech}</em></span>
+                                <span className='text-right'>{project.link}</span>
                             </div>
-                        )
-                    })}
-            </div>
-        )
-    };
+                            <ul className="mb-4 list-disc list-inside">
+                            {project.descriptions.map((description) => {
+                                return (
+                                    <li key={description.id}>{description.description}</li>
+                                )
+                            })}
+                            </ul>
+                        </div>
+                    )
+                })}
+        </div>
+    )
 }
 
 export default ResumeProjects;
