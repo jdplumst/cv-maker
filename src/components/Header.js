212ad9a822e7d059ff21d9aec3143432fdbca725
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-04 00:17:27 -0500

    Convert Header to functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,11 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Header extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <nav className="relative w-full flex flex-wrap items-center justify-center py-4 bg-gray-100 shadow-lg navbar navbar-expand-lg navbar-light">
-                <h1 className='text-4xl'>CV Maker</h1>
-            </nav>
-        )
-    }
+function Header() {
+    return (
+        <nav className="relative w-full flex flex-wrap items-center justify-center py-4 bg-gray-100 shadow-lg navbar navbar-expand-lg navbar-light">
+            <h1 className='text-4xl'>CV Maker</h1>
+        </nav>
+    )
 }
 
 export default Header;
