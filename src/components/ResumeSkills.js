fe5565a43404a475260ba21087bc8382358021ae
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-04 00:48:02 -0500

    Convert ResumeSkills to functional component

--- src/components/ResumeSkills.js
+++ src/components/ResumeSkills.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ResumeSkills extends React.Component {
-    constructor(props) {
-        super(props);
-    };
-
-    render() {
-        return (
-            <div>
-                <h3 className='text-2xl mt-4'>Skills</h3>
-                <hr className='border-black mb-4'></hr>
-                <p><strong>Langauges: </strong>{this.props.skills.languages}</p>
-                <p><strong>Libraries and Frameworks: </strong>{this.props.skills.libraries}</p>
-                <p><strong>Developer Tools: </strong>{this.props.skills.devtools}</p>
-            </div>
-        )
-    };
+function ResumeSkills(props) {
+    return (
+        <div>
+            <h3 className='text-2xl mt-4'>Skills</h3>
+            <hr className='border-black mb-4'></hr>
+            <p><strong>Langauges: </strong>{props.skills.languages}</p>
+            <p><strong>Libraries and Frameworks: </strong>{props.skills.libraries}</p>
+            <p><strong>Developer Tools: </strong>{props.skills.devtools}</p>
+        </div>
+    )
 }
 
 export default ResumeSkills;
