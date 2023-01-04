48ae21fd90070752fd91c8c7ea4d8650922d6f49
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-04 00:38:46 -0500

    Convert ResumePersonalInfo to functional component

--- src/components/ResumePersonalInfo.js
+++ src/components/ResumePersonalInfo.js
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ResumePersonalInfo extends React.Component {
-    constructor(props) {
-        super(props);
+function ResumePersonalInfo(props) {
+    // Make sure container is correct height
+    let h2;
+    if (props.personalInfo.firstname === '' && props.personalInfo.lastname === '') {
+        h2 = <h2 className='text-center text-3xl mb-4 h-36'></h2>
+    } else {
+        h2 = <h2 className='text-center text-3xl mb-4'>{props.personalInfo.firstname} {props.personalInfo.lastname}</h2>
     }
-
-    render() {
-        // Make sure container is correct height
-        let h2;
-        if (this.props.personalInfo.firstname === '' && this.props.personalInfo.lastname === '') {
-            h2 = <h2 className='text-center text-3xl mb-4 h-36'></h2>
-        } else {
-            h2 = <h2 className='text-center text-3xl mb-4'>{this.props.personalInfo.firstname} {this.props.personalInfo.lastname}</h2>
-        }
-        return (
-            <div>
-                {h2}
-                <div className='grid grid-flow-col divide-x divide-zinc-900 text-center'>
-                    <span className='pr-2'>{this.props.personalInfo.phone}</span>
-                    <span className='px-2'>{this.props.personalInfo.email}</span>
-                    <span className='px-2'>{this.props.personalInfo.linkedin}</span>
-                    <span className='pl-2'>{this.props.personalInfo.github}</span>
-                </div>
+    return (
+        <div>
+            {h2}
+            <div className='grid grid-flow-col divide-x divide-zinc-900 text-center'>
+                <span className='pr-2'>{props.personalInfo.phone}</span>
+                <span className='px-2'>{props.personalInfo.email}</span>
+                <span className='px-2'>{props.personalInfo.linkedin}</span>
+                <span className='pl-2'>{props.personalInfo.github}</span>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default ResumePersonalInfo;
